c343b29b358350bb4854cf4efea72acf2c278e0e
alvaroyuri <64052991+alvaroyuri@users.noreply.github.com>
2023-03-23 19:49:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,519 @@
+var fade = {
+    name: "fade",
+    attributes: { Beleza: 8, Skills: 8, Ultimate: 8 },
+    class: "initiator",
+    region: "turquia",
+    img: "https://t.ctcdn.com.br/iqV4kxWjrv6x9rmDUUOf5Fae5Ic=/1024x0/smart/filters:format(webp)/i584752.jpeg",
+};
+var neon = {
+    name: "neon",
+    attributes: { Beleza: 9, Skills: 6, Ultimate: 5 },
+    class: "duelist",
+    region: "filipinas",
+    img: "https://t.ctcdn.com.br/3DyBPPz5SFU-OR_XIdUdUSlTE6U=/1024x0/smart/filters:format(webp)/i592188.jpeg",
+};
+var chamber = {
+    name: "chamber",
+    attributes: { Beleza: 4, Skills: 8, Ultimate: 10 },
+    class: "sentinel",
+    region: "franca",
+    img: "https://t.ctcdn.com.br/lAkj1bFc6ytOiToCHn_PwGIgpzQ=/1024x0/smart/filters:format(webp)/i592102.png",
+};
+var kayo = {
+    name: "Kayo",
+    attributes: { Beleza: 3, Skills: 7, Ultimate: 7 },
+    class: "initiator",
+    region: "andromeda galaxy",
+    img: "https://t.ctcdn.com.br/9UlLqZqhsVCXS_vhmD2eCFvcqkk=/1024x0/smart/filters:format(webp)/i592187.jpeg",
+};
+var astra = {
+    name: "astra",
+    attributes: { Beleza: 10, Skills: 9, Ultimate: 6 },
+    class: "controller",
+    region: "gana",
+    img: "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt25b41d8e04d33db0/6042ad8308636f3d7749b8aa/3_9_21_AstraA15KeyArtBTSArticle_Astra_Wallpapers_purple.jpg",
+};
+var yoru = {
+    name: "yoru",
+    attributes: { Beleza: 2, Skills: 3, Ultimate: 4 },
+    class: "duelist",
+    region: "japao",
+    img: "https://t.ctcdn.com.br/l-2GyYCsOEZ9IZrjuAmD_NILTgE=/1024x0/smart/filters:format(webp)/i592107.jpeg",
+};
+var skye = {
+    name: "skye",
+    attributes: { Beleza: 7, Skills: 7, Ultimate: 6 },
+    class: "initiator",
+    region: "australia",
+    img: "https://t.ctcdn.com.br/TlXm016M_H3HhRtmQcWDn4g_has=/1024x0/smart/filters:format(webp)/i592108.jpeg",
+};
+var breach = {
+    name: "breach",
+    attributes: { Beleza: 3, Skills: 6, Ultimate: 6 },
+    class: "initiator",
+    region: "suecia",
+    img: "https://t.ctcdn.com.br/-1vI0vcdz5WtH8m3FSNDy0vCQoo=/1024x0/smart/filters:format(webp)/i592111.jpeg",
+};
+var raze = {
+    name: "raze",
+    attributes: { Beleza: 10, Skills: 6, Ultimate: 9 },
+    class: "duelist",
+    region: "brasil",
+    img: "https://t.ctcdn.com.br/mSsNlpxHFkKcfO4bMRcHSwGKYOs=/1024x0/smart/filters:format(webp)/i592113.jpeg",
+};
+var reyna = {
+    name: "reyna",
+    attributes: { Beleza: 10, Skills: 4, Ultimate: 5 },
+    class: "duelist",
+    region: "mexico",
+    img: "https://t.ctcdn.com.br/mRgKyUPy2_R3INT-2DC7o2m18BY=/1024x0/smart/filters:format(webp)/i592115.jpeg",
+};
+var jett = {
+    name: "jett",
+    attributes: { Beleza: 8, Skills: 8, Ultimate: 7 },
+    class: "duelist",
+    region: "coreia do sul",
+    img: "https://t.ctcdn.com.br/JEllLF5id4OQSxDlRIDZnIueqNc=/1024x0/smart/filters:format(webp)/i592118.jpeg",
+};
+var phoenix = {
+    name: "phoenix",
+    attributes: { Beleza: 5, Skills: 7, Ultimate: 8 },
+    class: "duelist",
+    region: "reino unido",
+    img: "https://t.ctcdn.com.br/AKSpevcdmPd-UOpeMzAngg6F59U=/1024x0/smart/filters:format(webp)/i592119.jpeg",
+};
+var sage = {
+    name: "sage",
+    attributes: { Beleza: 7, Skills: 9, Ultimate: 10 },
+    class: "sentinel",
+    region: "china",
+    img: "https://t.ctcdn.com.br/lb_MZWGBwFrUdsHZtaQTUCgcxVA=/1024x0/smart/filters:format(webp)/i592186.jpeg",
+};
+var sova = {
+    name: "sova",
+    attributes: { Beleza: 8, Skills: 8, Ultimate: 5 },
+    class: "initiator",
+    region: "russia",
+    img: "https://t.ctcdn.com.br/_LngJcsor4Jkwcnh6xivBNCiUMM=/1024x0/smart/filters:format(webp)/i592185.jpeg",
+};
+var cypher = {
+    name: "cypher",
+    attributes: { Beleza: 2, Skills: 5, Ultimate: 4 },
+    class: "sentinel",
+    region: "marrocos",
+    img: "https://t.ctcdn.com.br/wGpkiSxWzwCH5lLTxCKk2Omodmk=/1024x0/smart/filters:format(webp)/i592126.jpeg",
+};
+var killjoy = {
+    name: "killjoy",
+    attributes: { Beleza: 8, Skills: 7, Ultimate: 7 },
+    class: "sentinel",
+    region: "alemanha",
+    img: "https://t.ctcdn.com.br/xx8pqVCVyyCbLQ-dm2dHeuryZ48=/1024x0/smart/filters:format(webp)/i592128.jpeg",
+};
+var omen = {
+    name: "omen",
+    attributes: { Beleza: 3, Skills: 6, Ultimate: 3 },
+    class: "controller",
+    region: "shadow planet",
+    img: "https://t.ctcdn.com.br/4kw02l-EWHhoE2KVcIwjlf2fCSk=/1024x0/smart/filters:format(webp)/i592131.jpeg",
+};
+var viper = {
+    name: "viper",
+    attributes: { Beleza: 8, Skills: 9, Ultimate: 9 },
+    class: "controller",
+    region: "estados unidos",
+    img: "https://t.ctcdn.com.br/Zvh-tmRi6rRFxtmNh3CuSdIQkBE=/1024x0/smart/filters:format(webp)/i592135.jpeg",
+};
+var brimstone = {
+    name: "brimstone",
+    attributes: { Beleza: 8, Skills: 8, Ultimate: 6 },
+    class: "controller",
+    region: "estados unidos",
+    img: "https://t.ctcdn.com.br/9JBpe332pUJ8WS3-9Ff2XeXlO3s=/1024x0/smart/filters:format(webp)/i592136.jpeg",
+};
+var harbor = {
+    name: "harbor",
+    attributes: { Beleza: 7, Skills: 7, Ultimate: 5 },
+    class: "controller",
+    region: "india",
+    img: "https://stories.br.millenium.gg/valorant-harbor-e-desabilitado/assets/4.jpeg",
+};
+
+var card = [
+    harbor,
+    yoru,
+    viper,
+    sova,
+    skye,
+    sage,
+    reyna,
+    raze,
+    phoenix,
+    omen,
+    neon,
+    killjoy,
+    kayo,
+    jett,
+    fade,
+    cypher,
+    chamber,
+    brimstone,
+    breach,
+    astra,
+];
+
+var deckPlayer = [];
+var deckMachine = [];
+var handPlayer = [];
+var cardPlayer = 0;
+var cardMachine = 0;
+var moldura =
+    "<img src='https://i.ibb.co/Qnx4mjY/oie-transparent.png' class='deck4'>";
+var moldura2 =
+    "<img src='https://i.ibb.co/SPK85n9/Valorant-Mobile-Wallpaper-4k.jpg' class='deck4'>";
+
+function btnPull() {
+    //puxa as cartas
+    distributeDeck();
+
+    var elementoscorePlayer = document.getElementById("scorePlayer");
+
+    var elementoscorePc = document.getElementById("scorePc");
+
+    elementoscorePlayer.innerHTML = "";
+    elementoscorePlayer.innerHTML = `PLAYER - ${deckPlayer.length}`;
+
+    elementoscorePc.innerHTML = "";
+    elementoscorePc.innerHTML = `PC - ${deckMachine.length}`;
+
+    document.getElementById("btn-Puxar").disabled = true; //desabilita o botao puxar
+}
+
+function distributeDeck() {
+    //sorteia e distribui os cards
+
+    //embaralha a array card
+    card.sort(function () {
+        return Math.random() - 0.5;
+    });
+
+    var sizeDeck = card.length / 2;
+
+    //distribui deck player
+    for (var i = 0; i < sizeDeck; i++) {
+        deckPlayer[i] = card[i];
+    }
+
+    //distribui deck maquina
+    for (var i = 0; i < sizeDeck; i++) {
+        deckMachine[i] = card[i + sizeDeck];
+    }
+
+    distributeHand();
+}
+
+function distributeHand() {
+    //distribui as cartas na mao
+
+    if (deckPlayer.length <= 3) {
+        sizeHand = deckPlayer.length;
+    } else {
+        sizeHand = 3;
+    }
+
+    //distribui hand
+    for (var i = 0; i < sizeHand; i++) {
+        handPlayer[i] = deckPlayer[i];
+    }
+
+    displayCardPlayer();
+}
+
+function displayCardPlayer() {
+    //preenche as cartas da mao no html
+
+    //limpa o html
+    for (var i = 0; i < 3; i++) {
+        var selectDivHand = document.getElementById("hand" + i);
+
+        selectDivHand.innerHTML = "";
+        selectDivHand.style.backgroundImage = "";
+    }
+
+    //preenche o html
+    for (var i = 0; i < handPlayer.length; i++) {
+        var selectDivHand = document.getElementById("hand" + i);
+
+        selectDivHand.innerHTML = "";
+        selectDivHand.style.backgroundImage = "";
+
+        selectDivHand.style.backgroundImage =
+            "url('" + handPlayer[i].img + "')";
+
+        var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+        var chooseAttribute = "";
+
+        //laco de repeticao utilizado para mapear chaves de um objeto
+        for (var walksAttribute in handPlayer[i].attributes) {
+            chooseAttribute +=
+                "<input type='radio' name='opcao' value='" +
+                walksAttribute +
+                "'>" +
+                walksAttribute +
+                ": " +
+                handPlayer[i].attributes[walksAttribute] +
+                "<br>";
+        }
+
+        //notacao template string
+        var nameHTML = `<p class="carta-subtitle">${handPlayer[i].name}</p>`;
+
+        selectDivHand.innerHTML = "";
+        selectDivHand.innerHTML =
+            moldura + nameHTML + tagHTML + chooseAttribute + "</div>";
+    }
+}
+
+function selectCard() {
+    //move a carta selecionada para div carta-jogador
+
+    cleanHTML(); //limpa a div
+
+    var listCardSelect = getCard();
+
+    var selectDivHand = document.getElementById(listCardSelect[0].id);
+
+    var selectDivCardPlayer = document.getElementById("carta-jogador");
+
+    selectDivCardPlayer.style.backgroundImage =
+        selectDivHand.style.backgroundImage;
+
+    selectDivCardPlayer.innerHTML = "";
+    selectDivCardPlayer.innerHTML = selectDivHand.innerHTML;
+
+    //Após a carta selecionada ser repassada para o campo de batalha (classe carta-jogador), remarca a opção (skill, aparencia, ultimate) flegada anteriormente.
+    var flegOpcao = selectDivCardPlayer.querySelector(
+        `[name=opcao][value=${listCardSelect[1]}]`
+    );
+    flegOpcao.checked = true; //
+
+    //Localiza o nome da carta escolhida pelo player
+    var selectPCardName = selectDivCardPlayer.querySelector(
+        "[class=carta-subtitle]"
+    );
+
+    //busca o texto contido no elemento html procurado, utiliza o metodo trim() para remover espacos em branco
+    var elementCardName = selectPCardName.textContent.trim();
+
+    /*quebrei a cuca aqui, pois, estava inicialmente procurando o indice da carta desta forma >   handPlayer.indexOf(elementCardName). 
+    Nao funciona, pois, compara uma string (elementCardName) a uma lista de objetos (handPlayer). A solucao foi criar um laco de repeticao, que compare a string (elementCardName),
+    com  outra string (o valor da chave NAME dentro de cada objeto)
+    */
+    //procura a carta selecionada dentro da array handPlayer, atribui o objeto para variavel cardPlayer.
+
+    for (var i = 0; i < handPlayer.length; i++) {
+        if (handPlayer[i].name === elementCardName) {
+            cardPlayer = handPlayer[i];
+            break;
+        }
+    }
+
+    document.getElementById("btnJogar").disabled = false; //habilita o botao jogar
+    document.getElementById("selecionarCarta").disabled = true; //habilita o botao jogar
+
+    var elementoResultado = document.getElementById("resultado");
+    elementoResultado.innerHTML = "";
+}
+
+function getAttribute() {
+    var elementoOpcao = document.getElementsByName("opcao"); //procura a chave selecionada, percorres os 3 radios disponiveis | procura o atributo selecionado
+
+    for (i = 0; i < elementoOpcao.length; i++) {
+        if (elementoOpcao[i].checked) {
+            return elementoOpcao[i].value;
+        }
+    }
+}
+
+function play() {
+    displayCardMachine();
+
+    var selectAttribute = getAttribute(); //recebe o atributo selecionado
+
+    var attributePlayer = cardPlayer.attributes[selectAttribute]; //busca o VALOR do atributo, no objeto 1
+
+    var attributeMachine = cardMachine.attributes[selectAttribute]; //busca o VALOR do atributo, no objeto 2
+
+    var elementoResultado = document.getElementById("resultado");
+
+    var elementoscorePlayer = document.getElementById("scorePlayer");
+
+    var elementoscorePc = document.getElementById("scorePc");
+
+    //checa os resultados
+    if (attributePlayer == attributeMachine) {
+        elementoResultado.innerHTML = "<p class='draw'>Empate</p>";
+
+        deckPlayer.splice(deckPlayer.indexOf(cardPlayer), 1); //retira a carta utilizada do inicio do deck
+        deckPlayer.push(cardPlayer); //remaneja a minha utilizada o final do deck
+
+        deckMachine.splice(deckMachine.indexOf(cardMachine), 1); //retira a carta utilizada do inicio do deck
+        deckMachine.push(cardMachine); //remaneja a carta para o final do deck
+
+        handPlayer.splice(handPlayer.indexOf(cardPlayer), 1); //retira a carta da lista handPlayer
+        displayCardPlayer(); //atualiza o html com a hand
+
+        if (handPlayer == 0) {
+            distributeHand(); //
+        }
+    } else if (attributePlayer > attributeMachine) {
+        elementoResultado.innerHTML = "<p class='win'>Ganhou a rodada</p>";
+
+        deckPlayer.push(cardMachine); //pega a carta para o deck
+        deckMachine.splice(deckMachine.indexOf(cardMachine), 1); //retira a carta do deck
+
+        deckPlayer.splice(deckPlayer.indexOf(cardPlayer), 1); //retira a carta utilizada do inicio do deck
+        deckPlayer.push(cardPlayer); //remaneja a minha utilizada o final do deck
+
+        handPlayer.splice(handPlayer.indexOf(cardPlayer), 1); //retira a carta da lista handPlayer
+        displayCardPlayer(); //atualiza o html com a hand
+
+        if (handPlayer == 0) {
+            distributeHand();
+        }
+    } else if (attributePlayer < attributeMachine) {
+        elementoResultado.innerHTML = "<p class='lower'>Perdeu a rodada</p>";
+
+        deckMachine.push(cardPlayer); //pega a carta para o deck
+        deckPlayer.splice(deckPlayer.indexOf(cardPlayer), 1); //retira a carta do deck do player
+
+        deckMachine.splice(deckMachine.indexOf(cardMachine), 1); //retira a carta utilizada do inicio do deck
+        deckMachine.push(cardMachine); //remaneja a carta para o final do deck
+
+        handPlayer.splice(handPlayer.indexOf(cardPlayer), 1); //retira a carta da lista handPlayer
+        displayCardPlayer(); //atualiza o html com a hand
+
+        if (handPlayer == 0) {
+            distributeHand();
+        }
+    } else {
+        elementoResultado.innerHTML =
+            "<p class='resultado-final'>Erro na matrix</p>";
+    }
+
+    document.getElementById("btnJogar").disabled = false;
+
+    elementoscorePlayer.innerHTML = "";
+    elementoscorePlayer.innerHTML = `PLAYER - ${deckPlayer.length}`;
+
+    elementoscorePc.innerHTML = "";
+    elementoscorePc.innerHTML = `PC - ${deckMachine.length}`;
+
+    // checa o end game
+    if (deckMachine.length == 0) {
+        elementoResultado.innerHTML = "<p class='win'>VITÓRIA</p>";
+        document.getElementById("btnJogar").disabled = true;
+        document.getElementById("btn-Puxar").disabled = true;
+        document.getElementById("selecionarCarta").disabled = true;
+    } else if (deckPlayer == 0) {
+        elementoResultado.innerHTML = "<p class='lower'>DERROTA</p>";
+        document.getElementById("btnJogar").disabled = true;
+        document.getElementById("btn-Puxar").disabled = true;
+        document.getElementById("selecionarCarta").disabled = true;
+    } else {
+        document.getElementById("btnJogar").disabled = true;
+        document.getElementById("selecionarCarta").disabled = false;
+    }
+}
+
+function getCard() {
+    //procura o atributo selecionado
+
+    var opcaoHtml = document.getElementsByName("opcao");
+
+    for (i = 0; i < opcaoHtml.length; i++) {
+        if (opcaoHtml[i].checked) {
+            var div = opcaoHtml[i].closest("form > div > div > div"); //closest retorna um ancestral mais proximo em relação ao elemento atual, possui um seletor fornecido como parâmetro
+            var elementoOpcao = opcaoHtml[i].value;
+            var lista = [div, elementoOpcao];
+            console.log(lista[0]);
+
+            return lista;
+        }
+    }
+}
+
+function displayCardMachine() {
+    //exibe as cartas na tela
+
+    if (deckMachine.length > 0) {
+        cardMachine = deckMachine[0];
+    } else {
+        console.log("Acabou o deck da maquina");
+    }
+
+    var selectDivCardMachine = document.getElementById("carta-maquina");
+
+    selectDivCardMachine.style.backgroundImage = `url( ${cardMachine.img} )`;
+
+    selectDivCardMachine.style.backgroundImage =
+        "url('" + cardMachine.img + "')";
+
+    var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+    var chooseAttribute = "";
+
+    for (var walksAttribute in cardMachine.attributes) {
+        //laco de repeticao utilizado para mapear chaves de um objeto
+
+        chooseAttribute +=
+            "<p class ='lista' type='text' name='opcao' value='" +
+            walksAttribute +
+            "'>" +
+            walksAttribute +
+            ": " +
+            cardMachine.attributes[walksAttribute] +
+            "</p>";
+    }
+
+    var nameHTML = `<p class="carta-subtitle">${cardMachine.name}</p>`; //mais uma vez utilizando a notacao template string
+
+    selectDivCardMachine.innerHTML = "";
+    selectDivCardMachine.innerHTML =
+        moldura + nameHTML + tagHTML + chooseAttribute + "</div>";
+
+    console.log(selectDivCardMachine.innerHTML);
+
+    //console.log(handMachine);
+    //console.log(cardMachine);
+}
+
+function cleanHTML() {
+    var selectDivCardMachine = document.getElementById("carta-maquina");
+    selectDivCardMachine.style.backgroundImage = "";
+    selectDivCardMachine.innerHTML = moldura2;
+
+    var selectDivCardPlayer = document.getElementById("carta-jogador");
+    selectDivCardPlayer.style.backgroundImage = "";
+    selectDivCardPlayer.innerHTML = moldura2;
+}
+
+/*tasks:
+- reorganizar o codigo ok
+- desabilitar botoes no endgame ok
+- testes de endgame ok
+- sistema de 3 cards, escolha da carta - ok
+- remajeno de cartas de acordo com o resultado - ok
+- iniciar com deck de 10 cartas p/ cada. OK
+- criar arrays para cada jogador OK
+- funcao para dividir cartas OK
+- Sistema de ganhar cartas do outro player e quando zerar as cartas chama no endgame. ok
+- adicionar baralho ok
+- deixar o radio selecionado por default ok
+
+ideias:
+- responsividade
+- adicionar inteligencia para maquina escolher a melhor carta possivel dentro da hand
+- adicionar vez da maquina jogar
+*/
